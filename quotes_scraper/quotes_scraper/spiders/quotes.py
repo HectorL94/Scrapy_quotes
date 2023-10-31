b8b2be2b72e7231a4bf35810173d54824322b9d1
Hector Licea <halolicea@gmail.com>
2023-10-31 16:46:45 +0100

    Añadidos los resultados

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -9,24 +9,12 @@ class QuotesSpyder(scrapy.Spider):
     ]
 
     def parse(self,response):
-        print("\n\n")
-        print("*"*10)
-        
         title = response.xpath('//h1/a/text()').get()
-        print(f'Titulo: {title}')
-        print("\n\n")
-        print("*"*10)
-
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        print('Citas:')
-        for quote in quotes:
-            print(f'- {quote}')
-        print("\n\n")
-        print("*"*10)
-
         top_ten_tags = response.xpath('//div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()').getall()
-        for tag in top_ten_tags:
-            print(f'- {tag}')
-        #print(response.status, response.headers)
-        print("*"*10)
-        print("\n\n")
+
+        yield {
+            'title' : title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        }
