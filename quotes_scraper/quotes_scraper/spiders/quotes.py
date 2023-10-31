99bbe21bc34e02ca3eca17e92c8ee59e02250945
Hector Licea <halolicea@gmail.com>
2023-10-31 17:15:07 +0100

    Modifica para seguir la siguiente pagina

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -2,12 +2,16 @@ import scrapy
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags = //div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()
+# Next page button = //ul[@class="pager"]//a/@href
 class QuotesSpyder(scrapy.Spider):
     name = "quotes"
     start_urls = [
         "https://quotes.toscrape.com/"
     ]
-
+    custom_settings = {
+        'FEED_URI':'quotes.json',
+        'FEED_FORMAT': 'json'
+    }
     def parse(self,response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
@@ -18,3 +22,7 @@ class QuotesSpyder(scrapy.Spider):
             'quotes': quotes,
             'top_ten_tags': top_ten_tags
         }
+
+        next_page_button_link = response.xpath('//ul[@class="pager"]//a/@href').get()
+        if next_page_button_link: 
+            yield response.follow(next_page_button_link, callback=self.parse)
